d563e1ef8d6c3d7e3c283adf3c970a30048942d0
Carlo Mancini <carlo.mancini.terracciano@roma1.infn.it>
2018-12-21 12:10:43 +0100

    update

--- VAE_example_3d.py
+++ VAE_example_3d.py
@@ -1,10 +1,10 @@
 # Simple VAE implementation
 # Read MNIST dataset images, train a VAE, generate MNIST-like images
 from __future__ import  division, print_function
-# import os
+import os
 
-# os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
-# os.environ["CUDA_VISIBLE_DEVICES"]="1"
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 import keras
 from keras import layers
 from keras import backend as K
@@ -13,17 +13,13 @@ from keras.datasets import mnist
 from keras.optimizers import SGD, RMSprop, Adam
 import numpy as np
 
-import ROOT
-import root_numpy
-
 K.set_image_data_format('channels_last')
 
 #parameters
 #batch_size = 16
-batch_size = 4
-encoder_dim = 32
-latent_dim = 2  # Dimensionality of the latent space (a cube)
-img_shape = (128,128,128,2)#(128, 128, 128, 3) # shape of the input images (MNIST 25x25 pixels x 1 (gray scale))
+batch_size = 16
+latent_dim = 3  # Dimensionality of the latent space (a cube)
+img_shape = (28,28,28,2)#(128, 128, 128, 3) # shape of the input images (MNIST 25x25 pixels x 1 (gray scale))
 
 # Latent space sampling function
 def sampling(args):
@@ -56,19 +52,16 @@ input_img = keras.Input(shape=img_shape)
 #x = layers.Conv3D(8, 3, padding='same', activation='relu', strides=(2, 2, 2))(x)
 ## x = layers.Conv3D(64, 3, padding='same', activation='relu')(x)
 #x = layers.Conv3D(8, 3, padding='same', activation='relu')(x)
-x = layers.Conv3D(64, 3, padding='same', activation='relu')(input_img)
-x = layers.Conv3D(64, 3, padding='same', activation='relu', strides=(2,2,2))(x)
-x = layers.Conv3D(32, 3, padding='same', activation='relu')(x)
+x = layers.Conv3D(32, 3, padding='same', activation='relu')(input_img)
 x = layers.Conv3D(32, 3, padding='same', activation='relu', strides=(2,2,2))(x)
+#x = layers.Conv2D(64, 3, padding='same', activation='relu')(x)
 x = layers.Conv3D(32, 3, padding='same', activation='relu')(x)
 
 shape_before_flattening = K.int_shape(x)
 print("shape before flattening",shape_before_flattening)
 x = layers.Flatten()(x)
 print("shape alter flattening", K.int_shape(x))
-#dimensione spazio trasformato dell'encoder o dimensione dell'encoder
-#provare un numero piu' grande
-x = layers.Dense(encoder_dim, activation='tanh')(x)
+x = layers.Dense(16, activation='tanh')(x)
 
 #latent space model parametrised by a mean and a log_variance
 z_mean = layers.Dense(latent_dim)(x)
@@ -96,8 +89,6 @@ print("shape after upsample reshaping", K.int_shape(x))
 #x = layers.Conv3DTranspose(8, 3, padding='same', activation='relu', strides=(2, 2, 2))(x)
 #x = layers.Conv3D(1, 3, padding='same', activation='sigmoid')(x)
 x = layers.Conv3DTranspose(32, 3, padding='same', activation='relu', strides=(2,2,2))(x)
-x = layers.Conv3D(64, 3, padding='same', activation='relu')(x)
-x = layers.Conv3DTranspose(32, 3, padding='same', activation='relu', strides=(2,2,2))(x)
 x = layers.Conv3D(2, 3, padding='same', activation='sigmoid')(x)
 # We end up with a feature map of the same size as the original input.
 print("shape after upsample conv",K.int_shape(x))
@@ -119,39 +110,16 @@ vae.summary()
 
 # Read MNIST dataset, normalize it and reshape to fit the expected input (1,28,28)
 #(x_train, _), (x_test, y_test) = mnist.load_data()
-# x_train = np.load("out.npy")
-# x_test = np.load("out_test.npy")
+#x_train = np.load("out.npy")
+#x_test = np.load("out_test.npy")
 
-dataFile = "out.root"
-print("reading data file:",dataFile)
-inputfile = ROOT.TFile(dataFile,"READ")
 
-nTrainingSet = 900
-nTestSet = 100
-
-x_train = np.zeros([nTrainingSet,128,128,128,2])
-x_test  = np.zeros([nTestSet,128,128,128,2])
-
-for i in range(0,nTrainingSet+nTestSet):
-    hX = inputfile.Get("hX1;"+str(i+1))
-    hP = inputfile.Get("hP1;"+str(i+1))
-    x = root_numpy.hist2array(hX)
-    p = root_numpy.hist2array(hP)
-    x /= x.max()
-    p /= p.max()
-    if i < nTrainingSet:
-        x_train[i,:,:,:,0] = x
-        x_train[i,:,:,:,1] = p
-    else:
-        x_test[i-nTrainingSet,:,:,:,0] = x
-        x_test[i-nTrainingSet,:,:,:,1] = p
-
-# x_train = np.random.randint(255, size=(32, 28,28,28,2))
-# x_test = np.random.randint(255, size=(32, 28,28,28,2))
-# x_train = x_train.astype('float32') / 255.
-# #x_train = x_train.reshape(x_train.shape + (1,))
-# x_test = x_test.astype('float32') / 255.
-# #x_test = x_test.reshape(x_test.shape + (1,))
+x_train = np.random.randint(255, size=(32, 28,28,28,2))
+x_test = np.random.randint(255, size=(32, 28,28,28,2))
+x_train = x_train.astype('float32') / 255.
+#x_train = x_train.reshape(x_train.shape + (1,))
+x_test = x_test.astype('float32') / 255.
+#x_test = x_test.reshape(x_test.shape + (1,))
 
 #Train the VAE
 vae.fit(x=x_train, y=None,
@@ -161,72 +129,32 @@ vae.fit(x=x_train, y=None,
         validation_data=(x_test, None)
         )
 
-vae.save_weights("weights", overwrite=True)
-decoder.save_weights("decoder_weights", overwrite=True)
+# # Generate images
+# import matplotlib.pyplot as plt
+# from scipy.stats import norm
 
-# Generate images
-import matplotlib.pyplot as plt
-from scipy.stats import norm
+# # Display a 2D manifold of the MNIST digits 
+# n = 15  # figure with 15x15 digits
+# digit_size = 28
+# figure = np.zeros((digit_size * n, digit_size * n))
+# # Linearly spaced coordinates on the unit square were transformed
+# # through the inverse CDF (ppf) of the Gaussian
+# # to produce values of the latent variables z,
+# # since the prior of the latent space is Gaussian
+# grid_x = norm.ppf(np.linspace(0.05, 0.95, n))
+# grid_y = norm.ppf(np.linspace(0.05, 0.95, n))
 
-# Display a 2D manifold of the MNIST digits 
-n = 6  # figure with 15x15 digits
-digit_size = 28
-figure = np.zeros((digit_size * n, digit_size * n))
-# Linearly spaced coordinates on the unit square were transformed
-# through the inverse CDF (ppf) of the Gaussian
-# to produce values of the latent variables z,
-# since the prior of the latent space is Gaussian
-grid_x = norm.ppf(np.linspace(0.05, 0.95, n))
-grid_y = norm.ppf(np.linspace(0.05, 0.95, n))
+# for i, yi in enumerate(grid_x):
+#     for j, xi in enumerate(grid_y):
+#         z_sample = np.array([[xi, yi]])
+#         z_sample = np.tile(z_sample, batch_size).reshape(batch_size, 2)
+#         x_decoded = decoder.predict(z_sample, batch_size=batch_size)
+#         digit = x_decoded[0].reshape(digit_size, digit_size)
+#         figure[i * digit_size: (i + 1) * digit_size,
+#                j * digit_size: (j + 1) * digit_size] = digit
 
-out_size = 1
-
-pExtr = 600.
-xExtr = 60.
-
-print("Starting to generate...")
-outputFile = ROOT.TFile("vae_output.root","RECREATE")
-for i, yi in enumerate(grid_x):
-    # print("i:",i)
-    for j, xi in enumerate(grid_y):
-        # print("j:",j)        
-        z_sample = np.array([[xi, yi]])
-        z_sample = np.tile(z_sample, out_size).reshape(out_size, 2)
-        x_decoded = decoder.predict(z_sample, batch_size=batch_size)
-        for k in range(0, out_size):
-            # print("k:",k)        
-            hX = ROOT.TH3F("hX_"+str(k)+"_"+str(j)+"_"+str(i),
-                           "hX_"+str(k)+"_"+str(j)+"_"+str(i),
-                          128,-xExtr,+xExtr,
-                          128,-xExtr,+xExtr,
-                          128,-xExtr,+xExtr,)
-            hP = ROOT.TH3F("hP_"+str(k)+"_"+str(j)+"_"+str(i),
-                           "hP_"+str(k)+"_"+str(j)+"_"+str(i),
-                           128,-pExtr,+pExtr,
-                           128,-pExtr,+pExtr,
-                           128,-pExtr,+pExtr,)
-            print("x mean:",np.mean(x_decoded[k,:,:,:,0]))
-            print("x std:",np.std(x_decoded[k,:,:,:,0]))
-            print("x min:",np.min(x_decoded[k,:,:,:,0]))
-            print("x max:",np.max(x_decoded[k,:,:,:,0]))
-            print("p mean:",np.mean(x_decoded[k,:,:,:,1]))
-            print("p std:",np.std(x_decoded[k,:,:,:,1]))
-            print("p min:",np.min(x_decoded[k,:,:,:,1]))
-            print("p max:",np.max(x_decoded[k,:,:,:,1]))
-            root_numpy.array2hist(x_decoded[k,:,:,:,0],hX)
-            root_numpy.array2hist(x_decoded[k,:,:,:,1],hP)
-            hX.Write()
-            hP.Write()
-outputFile.Close()
-
-        # digit = x_decoded[0].reshape(digit_size, digit_size)
-        # figure[i * digit_size: (i + 1) * digit_size,
-        #        j * digit_size: (j + 1) * digit_size] = digit
-        
-
-# plt.figure(figsize=(10, 10))
-# plt.imshow(figure, cmap='Greys_r')
-# plt.savefig("vae_sim.pdf")
-
+# # plt.figure(figsize=(10, 10))
+# # plt.imshow(figure, cmap='Greys_r')
+# # plt.savefig("vae_sim.pdf")
 
 
