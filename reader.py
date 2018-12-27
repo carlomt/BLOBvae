dbe0706f93dba5449d335368b288c7b7270cc8ec
Carlo Mancini <carlo.mancini.terracciano@roma1.infn.it>
2018-12-27 18:09:57 +0100

    update

--- reader.py
+++ reader.py
@@ -3,36 +3,38 @@ import ROOT
 import numpy as np
 import root_numpy
 
-inputfile = ROOT.TFile("out.root","READ")
+inputfile = ROOT.TFile("/Users/carlo/repos/c12fragmentation-2-build2/out.root","READ")
 
 nTrainingSet = 900
 nTestSet = 100
 
-tree = f.Get("tree")
+tree = inputfile.Get("tree")
 
-x_extra_training = np.zeros(nTrainingSet,15)
-x_extra_test = np.zeros(nTestSet,15)
+x_extra_training = np.zeros([nTrainingSet,15])
+x_extra_test = np.zeros([nTestSet,15])
 for i, event in enumerate(tree):
     j = 0
     if i < nTrainingSet: 
         x_extra = x_extra_training
+        ii = i
     else:
         x_extra = x_extra_test
-
-    x_extra[i,j] = tree.evt
+        ii = i-nTrainingSet
+        
+    x_extra[ii,j] = tree.evt
     j +=1
-    x_extra[i,j] = tree.b
+    x_extra[ii,j] = tree.b
     j +=1
-    x_extra[i,j] = tree.nfrg
+    x_extra[ii,j] = tree.nfrg
     j +=1
     for ifrag in range(0,tree.nfrg):
-        x_extra[i,j] = tree.Af[ifrag]
+        x_extra[ii,j] = tree.Af[ifrag]
         j +=1
-        x_extra[i,j] = tree.zf[ifrag]
+        x_extra[ii,j] = tree.zf[ifrag]
         j +=1
-        x_extra[i,j] = tree.Pzf[ifrag]
+        x_extra[ii,j] = tree.Pzf[ifrag]
         j +=1
-        x_extra[i,j] = tree.Eecc[ifrag]
+        x_extra[ii,j] = tree.Eecc[ifrag]
         j +=1
 
 np.save("x_extra_test.npy",x_extra_test)
@@ -43,8 +45,8 @@ x_train = np.zeros([nTrainingSet,128,128,128,2])
 x_test  = np.zeros([nTestSet,128,128,128,2])
 
 for i in range(0,nTrainingSet+nTestSet):
-    hX = inputfile.Get("hX1;"+str(i+1))
-    hP = inputfile.Get("hP1;"+str(i+1))
+    hX = inputfile.Get("hX0;"+str(i+1))
+    hP = inputfile.Get("hP0;"+str(i+1))
     x = root_numpy.hist2array(hX)
     p = root_numpy.hist2array(hP)
     x /= x.max()
